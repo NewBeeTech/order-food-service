30191feeed4a41e8273c346a098b299b228cb449
lijingjing12 <lijingjing7@jd.com>
2017-10-26 16:24:39 +0800

    [update] 修改餐厅列表返回的数据格式

--- controller/restaurant.js
+++ controller/restaurant.js
@@ -5,25 +5,27 @@ module.exports = {
    * 根据城市筛选餐厅列表
    */
   listByCity: function(req, res, next) {
-    var data = [
-      {
-        "_id": "xxx1",
-        "name": "Byran",
-        "cuisines": ["美国菜"],
-        "rating": 4.5,
-        "priceLevel": 3,
-        "city": {
-          "name": "New York",
-          "chineseName": "纽约",
-        },
-        "country": {
-          "name": "US",
-          "chineseName": "美国",
-        },
-        "mainImage": "https://pro.modao.cc/uploads3/" +
-        "images/1289/12896118/raw_1505974928.jpeg"
-      }
-    ]
+    var data = {
+      "rows": [
+        {
+          "_id": "xxx1",
+          "name": "Byran",
+          "cuisines": ["美国菜"],
+          "rating": 4.5,
+          "priceLevel": 3,
+          "city": {
+            "name": "New York",
+            "chineseName": "纽约"
+          },
+          "country": {
+            "name": "US",
+            "chineseName": "美国"
+          },
+          "mainImage": "https://pro.modao.cc/uploads3/images/1289/12896118/raw_1505974928.jpeg"
+        }
+      ],
+      "total": 28
+    }
     common.successRes(res, data)
   },
 
