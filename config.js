df987314296f72907319e4706070017dd4a65b2a
lijingjing12 <lijingjing7@jd.com>
2017-12-07 10:10:15 +0800

    [bugfix] add comma in config

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ module.exports = {
   // TODO: 部署时请根据需要更改配置
   db: {
     // url: 'mongodb://root:*******@dds-bp12f333bc784c141173.mongodb.rds.aliyuncs.com:3717,' +
-    // 'dds-bp12f333bc784c142459.mongodb.rds.aliyuncs.com:3717/admin?replicaSet=mgset-4725333'
+    // 'dds-bp12f333bc784c142459.mongodb.rds.aliyuncs.com:3717/admin?replicaSet=mgset-4725333',
     url: 'mongodb://localhost/food',
     poolSize: 6,
   },
